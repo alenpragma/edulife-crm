7bf104e1961d556760c06da20d1ce4fdcb9778cc
Md Asaduzaman <asadasad9282@gmail.com>
2024-02-10 18:37:36 +0600

    chages

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,6 @@ import { Server } from 'http';
 import mongoose from 'mongoose';
 import app from './app';
 import config from './config';
-import { logger } from './shared/logger';
 // import { errorlogger, logger } from './shared/logger';
 
 async function bootstrap() {
@@ -11,13 +10,13 @@ async function bootstrap() {
 
   const server: Server = app.listen(config.port, () => {
     // console.log(`Server running on port ${config.port}`);
-    logger.info(`Server running on port ${config.port}`);
+    // logger.info(`Server running on port ${config.port}`);
   });
 
   const exitHandler = () => {
     if (server) {
       server.close(() => {
-        logger.info('Server closed');
+        // logger.info('Server closed');
         console.log('Server closed');
       });
     }
